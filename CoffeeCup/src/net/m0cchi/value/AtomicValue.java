e47a04a0fcbff1793bdea8f316f8b85060de99ca
m0cchi <boom.boom.planet@gmail.com>
2016-07-31 12:59:03 +0900

    fix args

--- CoffeeCup/src/net/m0cchi/value/AtomicValue.java
+++ CoffeeCup/src/net/m0cchi/value/AtomicValue.java
@@ -1,5 +1,11 @@
 package net.m0cchi.value;
 
 public class AtomicValue {
-
+	private AtomicType type;
+	private String nativeValue;
+	
+	public AtomicValue(AtomicType type,String nativeValue) {
+		this.type = type;
+		this.nativeValue = nativeValue;
+	}
 }
