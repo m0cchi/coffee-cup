ee88ccb3c53ef69bea7d1f98d9317f30bbafecdb
m0cchi <boom.boom.planet@gmail.com>
2016-08-06 12:18:36 +0900

    remove unused value

--- CoffeeCup/src/net/m0cchi/value/AtomicValue.java
+++ CoffeeCup/src/net/m0cchi/value/AtomicValue.java
@@ -1,19 +1,10 @@
 package net.m0cchi.value;
 
-public class AtomicValue extends Value {
-	private String nativeValue;
+public class AtomicValue extends Value<String> {
 	
 	public AtomicValue(AtomicType type,String nativeValue) {
 		this.type = type;
-		this.nativeValue = nativeValue;
-	}
-
-	public String getNativeValue() {
-		return nativeValue;
-	}
-
-	public void setNativeValue(String nativeValue) {
-		this.nativeValue = nativeValue;
+		this.value = nativeValue;
 	}
 	
 }
