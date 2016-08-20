feb934678c991d8b01e604605f21b3bad185380d
m0cchi <boom.boom.planet@gmail.com>
2016-08-20 18:34:46 +0900

    impl kill func

---
+++ CoffeeCup/src/net/m0cchi/function/handler/Kill.java
@@ -0,0 +1,33 @@
+package net.m0cchi.function.handler;
+
+import net.m0cchi.exception.handler.Abort;
+import net.m0cchi.exception.handler.Signal;
+import net.m0cchi.value.Element;
+import net.m0cchi.value.Environment;
+import net.m0cchi.value.Function;
+import net.m0cchi.value.Value;
+
+public class Kill extends Function {
+	private static final long serialVersionUID = -7709583677888529462L;
+
+	public Kill() {
+		setArgs("kill target");
+	}
+
+	@Override
+	public Element invoke(Environment environment) {
+		Value<?> targetValue = (Value<?>) environment.getValue(getArgs()[0]);
+		Object target = targetValue.getNativeValue();
+		if (target instanceof Signal) {
+			throw (Signal) target;
+		} else {
+			try {
+				Class<?> clazz = Class.forName(target.toString());
+				throw (Signal) clazz.newInstance();
+			} catch (Exception e) {
+				throw new Abort();
+			}
+		}
+	}
+
+}
