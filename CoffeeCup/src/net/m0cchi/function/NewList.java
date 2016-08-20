34eb06e933453dfc8d1757a1931baa93d1de3a3c
m0cchi <boom.boom.planet@gmail.com>
2016-08-21 01:37:18 +0900

    impl list-fn

---
+++ CoffeeCup/src/net/m0cchi/function/NewList.java
@@ -0,0 +1,21 @@
+package net.m0cchi.function;
+
+import net.m0cchi.value.Element;
+import net.m0cchi.value.Environment;
+import net.m0cchi.value.Function;
+import net.m0cchi.value.SList;
+
+public class NewList extends Function {
+	private static final long serialVersionUID = 979011968472839107L;
+
+	public NewList() {
+		setArgs(REST, "new list");
+	}
+
+	@Override
+	public Element invoke(Environment environment) {
+		SList list = (SList) environment.getValue(getArgs()[1]);
+		return list;
+	}
+
+}
